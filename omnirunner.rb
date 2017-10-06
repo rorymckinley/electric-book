4f35b084f4b9efc5e27ab57f83622e1569c1a154
Rory McKinley <rorymckinley@gmail.com>
2017-10-06 06:59:47 +0200

    Copy across styles

--- omnirunner.rb
+++ omnirunner.rb
@@ -2,6 +2,13 @@
 
 require 'highline'
 require 'open3'
+require 'fileutils'
+
+def directory_has_contents?(path)
+  # To deal with Windows and *nix-like
+  # Ruby 2.4 has Dir.empty? which is a bit cleaner, but 2.4 only
+  Dir.entries(path).any? { |i| i != '.' && i != '..' }
+end
 
 cli = HighLine.new
 
@@ -62,3 +69,38 @@ stdout, stderr, _ = Open3.capture3(%Q{bundle exec jekyll build --config="_config
 cli.say("output #{stdout}")
 cli.say("errors #{stderr}\n")
 cli.say("HTML generated")
+
+# What's next?
+cli.say('Assembling epub...')
+
+# Copy styles, images, text and package.opf to epub folder.
+# The echo f preemptively answers xcopy's question whether
+# this is a file (see https://stackoverflow.com/a/3018371).
+# The > nul supresses command-line feedback (pseudo silent mode)
+
+# Test copying options
+cli.say('Copying styles...')
+if subdirectory
+  styles_path = File.join('_site', bookfolder, subdirectory, 'styles')
+  if Dir.exist?(styles_path) and directory_has_contents?(styles_path)
+    # Translation output, and an styles subdir for that translation exists:
+    # create folder structure, and copy only the styles in that translation folder
+    # Copy translated styles, after deleting original styles
+    FileUtils.remove_entry_secure(File.join('_site', bookfolder, 'styles'))
+    styles_dest_path = File.join('_site', 'epub', subdirectory, 'styles')
+    FileUtils.mkdir_p(styles_dest_path)
+    FileUtils.cp(Dir.glob(File.join(styles_path, '*.css')), styles_dest_path)
+  else
+    # Translation output, but no translated-styles subdirectory for that translation:
+    # copy the original styles files only
+    styles_dest_path = File.join('_site', 'epub', 'styles')
+    FileUtils.mkdir(styles_dest_path)
+    FileUtils.cp(Dir.glob(File.join('_site', bookfolder, 'styles', '*.css')), styles_dest_path)
+  end
+else
+  # If original language output: copy only files in fonts/epub
+  styles_dest_path = File.join('_site', 'epub', 'styles')
+  FileUtils.mkdir(styles_dest_path)
+  FileUtils.cp(Dir.glob(File.join('_site', bookfolder, 'styles', '*.css')), styles_dest_path)
+end
+cli.say('Styles copied')
